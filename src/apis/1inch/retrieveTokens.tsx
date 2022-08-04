c615601ef53aa2be340d2a990c9e09259e0d90dc
Shazman96 <admin@dcastack.com>
2022-08-04 13:54:20 -0400

    Integrate 1inch api

---
+++ src/apis/1inch/retrieveTokens.tsx
@@ -0,0 +1,26 @@
+import useSWR from "swr";
+import React, { useState } from "react";
+
+export default function use1inchRetrieveTokens(currentChain: number) {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { data, error } = useSWR(
+    `https://api.1inch.io/v4.0/${currentChain}/tokens`,
+    fetcher
+  );
+
+  if (data) {
+    if (data.tokens) {
+      data.strippedTokens = Object.values(data.tokens);
+      //   console.log("1inch fetch tokens success", data);
+    }
+  }
+
+  if (error) console.log("1inch fetch tokens error", error);
+
+  return {
+    tokens: data?.strippedTokens,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
