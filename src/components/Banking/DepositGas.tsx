e70d7a986fd404335dfe4df84322453991dbc2c4
Shazman96 <admin@dcastack.com>
2022-08-17 22:37:47 -0400

    Seperate deposit gas component + flow

--- src/components/Banking/DepositGas.tsx
+++ src/components/Banking/DepositGas.tsx
@@ -1,30 +1,19 @@
-import { useEffect, useState, useContext, ChangeEvent } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 
 import {
   Group,
   TextInput,
-  Grid,
   Container,
   Button,
   createStyles,
-  Avatar,
-  Space,
 } from "@mantine/core";
-import { showNotification, updateNotification } from "@mantine/notifications";
-import { CircleCheck, AlertOctagon } from "tabler-icons-react";
+import { showNotification } from "@mantine/notifications";
+import { AlertOctagon } from "tabler-icons-react";
 import GasToken from "../TokenDisplay/GasToken";
 
-import {
-  usePrepareContractWrite,
-  useContractWrite,
-  useAccount,
-  useBalance,
-  useWaitForTransaction,
-} from "wagmi";
-import { parseEther, formatEther } from "ethers/lib/utils";
-import { useAddRecentTransaction } from "@rainbow-me/rainbowkit";
-import { ContractContext } from "../../App";
+import { formatEther } from "ethers/lib/utils";
 import { BigNumber } from "ethers";
+import DepositGasFlow from "./DepositGasFlow";
 
 const useStyles = createStyles((theme) => ({
   input: {
@@ -39,12 +28,8 @@ interface ISetup {
 export default function DepositGas({
   weiDefaultValue = BigNumber.from(0),
 }: ISetup) {
-  const { address: contractAddr, abi: contractABI } =
-    useContext(ContractContext);
   const [depositAmount, setDeposit] = useState("0");
   const { classes } = useStyles();
-  const { address, isConnecting, isDisconnected } = useAccount();
-  const addRecentTransaction = useAddRecentTransaction();
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const re = /^\d*\.?\d*$/;
@@ -57,109 +42,7 @@ export default function DepositGas({
     setDeposit(formatEther(weiDefaultValue.toString()));
   }, [weiDefaultValue]);
 
-  const {
-    config: depositGasSetup,
-    error: depositGasError,
-    isError: prepareDepositGasError,
-  } = usePrepareContractWrite({
-    addressOrName: contractAddr,
-    contractInterface: contractABI,
-    enabled: depositAmount !== "" ? true : false,
-    functionName: "depositGas",
-    overrides: {
-      from: address,
-      value: depositAmount !== "" ? parseEther(depositAmount) : parseEther("0"),
-    },
-    onError(error) {
-      console.log("Deposit Gas Prepared Error", error);
-    },
-    onSuccess(data) {
-      console.log("Deposit Gas Prepared Success", data);
-    },
-  });
-
-  const {
-    data,
-    error,
-    isError: depositError,
-    write: depositGas,
-  } = useContractWrite({
-    ...depositGasSetup,
-    onSuccess(data) {
-      console.log("Deposit Gas Write Success", data);
-
-      showNotification({
-        id: "deposit-gas-pending",
-        loading: true,
-        title: "Pending Gas Deposit",
-        message: "Waiting for your tx. Check status on your account tab.",
-        autoClose: true,
-        disallowClose: false,
-      });
-    },
-
-    onError(error) {
-      console.log("Deposit Gas Write Error", error);
-
-      showNotification({
-        id: "deposit-gas-error",
-        color: "red",
-        title: "Error Gas Deposit",
-        message: "If this was unexpected, please raise an issue on github!",
-        autoClose: true,
-        disallowClose: false,
-        icon: <AlertOctagon />,
-      });
-    },
-  });
-
-  const { isLoading: txPending, isSuccess: txDone } = useWaitForTransaction({
-    hash: data?.hash,
-    onSuccess(data) {
-      console.log("Deposit Gas Success", data);
-
-      addRecentTransaction({
-        hash: data.transactionHash,
-        description: "Deposit Gas",
-      });
-
-      updateNotification({
-        id: "deposit-gas-pending",
-        color: "teal",
-        title: "Gas Deposit Received",
-        message: "Happy DCAing :)",
-        icon: <CircleCheck />,
-      });
-    },
-    onError(error) {
-      console.log("Deposit Gas Error", error);
-
-      updateNotification({
-        id: "deposit-gas-pending",
-        color: "red",
-        title: "Error Gas Deposit",
-        message: "If this was unexpected, please raise an issue on github!",
-        autoClose: true,
-        disallowClose: false,
-        icon: <AlertOctagon />,
-      });
-    },
-  });
-
-  const {
-    data: maxDeposit,
-    isError,
-    isLoading,
-  } = useBalance({
-    addressOrName: address,
-    watch: true,
-    onSuccess(data) {
-      console.log("Get User Wallet Gas Balance Success", data);
-    },
-    onError(error) {
-      console.log("Get User Wallet Gas Balance Error", error);
-    },
-  });
+  let depositGasActions = DepositGasFlow(depositAmount);
 
   return (
     <Container my="deposit_gas">
@@ -185,8 +68,10 @@ export default function DepositGas({
               radius="xl"
               size="md"
               onClick={() =>
-                maxDeposit
-                  ? setDeposit(formatEther(maxDeposit?.value.toString()))
+                depositGasActions?.max
+                  ? setDeposit(
+                      formatEther(depositGasActions?.max?.value.toString())
+                    )
                   : setDeposit("0")
               }
             >
@@ -201,7 +86,7 @@ export default function DepositGas({
           radius="xs"
           size="xl"
           onClick={() => {
-            if (maxDeposit?.formatted === "0.0") {
+            if (depositGasActions?.max?.formatted === "0.0") {
               showNotification({
                 id: "deposit-balance-error",
                 color: "red",
@@ -213,7 +98,7 @@ export default function DepositGas({
                 icon: <AlertOctagon />,
               });
             } else {
-              depositGas?.();
+              depositGasActions?.deposit?.();
             }
           }}
         >
---
+++ src/components/Banking/DepositGasFlow.tsx
@@ -0,0 +1,131 @@
+import { useContext } from "react";
+
+import { showNotification, updateNotification } from "@mantine/notifications";
+import { CircleCheck, AlertOctagon } from "tabler-icons-react";
+
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useAccount,
+  useBalance,
+  useWaitForTransaction,
+} from "wagmi";
+import { parseEther } from "ethers/lib/utils";
+import { useAddRecentTransaction } from "@rainbow-me/rainbowkit";
+import { ContractContext } from "../../App";
+
+export default function DepositGasFlow(defaultValue: string) {
+  const { address: contractAddr, abi: contractABI } =
+    useContext(ContractContext);
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const addRecentTransaction = useAddRecentTransaction();
+
+  const {
+    config: depositGasSetup,
+    error: depositGasError,
+    isError: prepareDepositGasError,
+  } = usePrepareContractWrite({
+    addressOrName: contractAddr,
+    contractInterface: contractABI,
+    enabled: defaultValue !== "" ? true : false,
+    functionName: "depositGas",
+    overrides: {
+      from: address,
+      value: defaultValue !== "" ? parseEther(defaultValue) : parseEther("0"),
+    },
+    onError(error) {
+      console.log("Deposit Gas Prepared Error", error);
+    },
+    onSuccess(data) {
+      console.log("Deposit Gas Prepared Success", data);
+    },
+  });
+
+  const {
+    data,
+    error,
+    isError: depositError,
+    write: depositGas,
+  } = useContractWrite({
+    ...depositGasSetup,
+    onSuccess(data) {
+      console.log("Deposit Gas Write Success", data);
+
+      showNotification({
+        id: "deposit-gas-pending",
+        loading: true,
+        title: "Pending Gas Deposit",
+        message: "Waiting for your tx. Check status on your account tab.",
+        autoClose: true,
+        disallowClose: false,
+      });
+    },
+
+    onError(error) {
+      console.log("Deposit Gas Write Error", error);
+
+      showNotification({
+        id: "deposit-gas-error",
+        color: "red",
+        title: "Error Gas Deposit",
+        message: "If this was unexpected, please raise an issue on github!",
+        autoClose: true,
+        disallowClose: false,
+        icon: <AlertOctagon />,
+      });
+    },
+  });
+
+  const { isLoading: txPending, isSuccess: txDone } = useWaitForTransaction({
+    hash: data?.hash,
+    onSuccess(data) {
+      console.log("Deposit Gas Success", data);
+
+      addRecentTransaction({
+        hash: data.transactionHash,
+        description: "Deposit Gas",
+      });
+
+      updateNotification({
+        id: "deposit-gas-pending",
+        color: "teal",
+        title: "Gas Deposit Received",
+        message: "Happy DCAing :)",
+        icon: <CircleCheck />,
+      });
+    },
+    onError(error) {
+      console.log("Deposit Gas Error", error);
+
+      updateNotification({
+        id: "deposit-gas-pending",
+        color: "red",
+        title: "Error Gas Deposit",
+        message: "If this was unexpected, please raise an issue on github!",
+        autoClose: true,
+        disallowClose: false,
+        icon: <AlertOctagon />,
+      });
+    },
+  });
+
+  const {
+    data: maxDeposit,
+    isError,
+    isLoading,
+  } = useBalance({
+    addressOrName: address,
+    watch: true,
+    onSuccess(data) {
+      console.log("Get User Wallet Gas Balance Success", data);
+    },
+    onError(error) {
+      console.log("Get User Wallet Gas Balance Error", error);
+    },
+  });
+
+  return {
+    deposit: depositGas,
+    max: maxDeposit,
+  };
+}
