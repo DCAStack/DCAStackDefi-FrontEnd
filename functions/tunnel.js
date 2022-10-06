0ae2107e94fb79dbe6d142b03431172820d05be3
ShamanShaz <admin@dcastack.com>
2022-10-06 19:16:28 -0400

    Update

--- functions/tunnel.js
+++ functions/tunnel.js
@@ -4,7 +4,7 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-async function handleRequest(request) {
+export async function handleRequest(request) {
   const url = new URL(request.url)
 
   // Handle requests for Sentry CDN JavaScript
