b00982cfab98bbb4ce53ecc7230a9e2c5ce02b6a
ShamanShaz <admin@dcastack.com>
2022-10-06 19:34:17 -0400

    Test

--- functions/tunnel.js
+++ functions/tunnel.js
@@ -9,67 +9,49 @@ export async function onRequest(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
 
-    console.log("We got stuff!");
-    console.log("request is", request)
-    console.log("env is", env)
-    console.log("params is", params)
-    console.log("waitUntil is", waitUntil)
-    console.log("data is", data)
+    const SLUG = '/tunnel/';
+    const url = new URL(request.url)
   
-  
-    return new Response("Hello, world!");
-  
+    if (request.method === 'GET' && url.pathname.startsWith(SLUG) && (url.pathname.endsWith('.js') || url.pathname.endsWith('.js.map'))) {
+        const path = url.pathname.slice(SLUG.length);
+        // Fetch and pass the same response, including headers
+        return fetch(`https://browser.sentry-cdn.com/${path}`);
+      }
+
+      if (request.method === 'POST' && url.pathname === SLUG) {
+        let { readable, writable } = new TransformStream()
+        request.body.pipeTo(writable);
+    
+        // We tee the stream so we can pull the header out of one stream 
+        // and pass the other straight as the fetch POST body 
+        const [header, body] = readable.tee();
+    
+        let decoder = new TextDecoder()
+        let chunk = '';
+    
+        const headerReader = header.getReader();
+    
+        while (true) {
+          const { done, value } = await headerReader.read();
+    
+          if (done) {
+            break;
+          }
+    
+          chunk += decoder.decode(value);
+          
+          const index = chunk.indexOf('\n');
+    
+          if (index >= 0) {
+            // Get the first line
+            const firstLine = chunk.slice(0, index);
+            const event = JSON.parse(firstLine);
+            const dsn = new URL(event.dsn);
+            // Post to the Sentry endpoint!
+            return fetch(`https://${dsn.host}/api${dsn.pathname}/envelope/`, { method: 'POST', body });
+          }
+        }
+      }
+
+      return new Response(null, { status: 404 });
   }
-  
-
-
-
-
-// export async function handleRequest(request) {
-//   const url = new URL(request.url)
-
-//   // Handle requests for Sentry CDN JavaScript
-//   if (request.method === 'GET' && url.pathname.startsWith(SLUG) && (url.pathname.endsWith('.js') || url.pathname.endsWith('.js.map'))) {
-//     const path = url.pathname.slice(SLUG.length);
-//     // Fetch and pass the same response, including headers
-//     return fetch(`https://browser.sentry-cdn.com/${path}`);
-//   }
-
-//   if (request.method === 'POST' && url.pathname === SLUG) {
-//     let { readable, writable } = new TransformStream()
-//     request.body.pipeTo(writable);
-
-//     // We tee the stream so we can pull the header out of one stream 
-//     // and pass the other straight as the fetch POST body 
-//     const [header, body] = readable.tee();
-
-//     let decoder = new TextDecoder()
-//     let chunk = '';
-
-//     const headerReader = header.getReader();
-
-//     while (true) {
-//       const { done, value } = await headerReader.read();
-
-//       if (done) {
-//         break;
-//       }
-
-//       chunk += decoder.decode(value);
-      
-//       const index = chunk.indexOf('\n');
-
-//       if (index >= 0) {
-//         // Get the first line
-//         const firstLine = chunk.slice(0, index);
-//         const event = JSON.parse(firstLine);
-//         const dsn = new URL(event.dsn);
-//         // Post to the Sentry endpoint!
-//         return fetch(`https://${dsn.host}/api${dsn.pathname}/envelope/`, { method: 'POST', body });
-//       }
-//     }
-//   }
-
-//   // If the above routes don't match, return 404
-//   return new Response(null, { status: 404 });
-// }
