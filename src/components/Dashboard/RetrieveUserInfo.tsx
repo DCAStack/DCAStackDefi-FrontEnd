0e750659340716850679e4afbb6899c62e7dd938
ShamanShaz <admin@dcastack.com>
2022-10-05 20:48:29 -0400

    Better handling + temp fix

--- src/components/Dashboard/RetrieveUserInfo.tsx
+++ src/components/Dashboard/RetrieveUserInfo.tsx
@@ -32,7 +32,7 @@ function RetrieveUserInfo() {
     functionName: "getUserAllTokenBalances",
     cacheOnBlock: true,
     watch: true,
-    enabled: address !== undefined && currentChain == 5, //TODO
+    enabled: address !== undefined,
     overrides: { from: address },
     onSuccess(data) {
       console.log("Get user token balances success", data);
@@ -40,6 +40,7 @@ function RetrieveUserInfo() {
     },
     onError(error) {
       console.error("Get user token balances Error", error);
+      setUserTokenInfo([[], [], []]);
     },
   });
 
@@ -50,13 +51,14 @@ function RetrieveUserInfo() {
     args: address,
     cacheOnBlock: true,
     watch: true,
-    enabled: address !== undefined && currentChain == 5, //TODO
+    enabled: address !== undefined,
     onSuccess(data) {
       console.log("Get user schedules success", data);
       setUserScheduleInfo(data);
     },
     onError(error) {
       console.error("Get user schedules Error", error);
+      setUserScheduleInfo([[], [], []]);
     },
   });
 
@@ -87,7 +89,8 @@ function RetrieveUserInfo() {
     joinNeededTokens[2] = joinNeededTokens[2].concat(userTokenBalances[2]);
   }
 
-  if (joinNeededTokens) {
+  if (joinNeededTokens && currentChain === 5) {
+    //TODO temp fix for crash
     joinNeededTokens[0].forEach(function (tokenAddr: string, index: number) {
       if (tokenAddr) {
         let tokenDetails = masterTokenList?.tokens[tokenAddr?.toLowerCase()];
